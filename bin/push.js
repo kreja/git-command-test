a05781991ba12cd17d0800b4ad8ef03d5a16c6c7
kaili.wkl <kaili.wkl@alibaba-inc.com>
2016-10-21 15:41:30 +0800

    update

--- bin/push.js
+++ bin/push.js
@@ -1,32 +1,20 @@
 #!/usr/bin/env node
 
-// var name = process.argv[2];
-// var exec = require('child_process').exec;
-
-// var child = exec('echo hello ' + name, function(err, stdout, stderr) {
-//   if (err) throw err;
-//   console.log(stdout);
-// });
-
-
-// todo:: daily/0.0.1 可以自己获取
 
 // push -origin daily/0.0.1 -msg ‘xx’
 // push -o daily/0.0.1 -m ‘xx’
 
 require('shelljs/global');
+var fs = require('fs');
 
 var argv = require('yargs')
-  .command("morning", "good morning", function (yargs) {
-      console.log("Good Morning");
-    })
-  .option('o', {
-    alias : 'origin',
-    demand: false,
-    // default: 'tom',
-    describe: '分支',
-    type: 'string'
-  })
+  // .option('o', {
+  //   alias : 'origin',
+  //   demand: false,
+  //   // default: 'tom',
+  //   describe: '分支',
+  //   type: 'string'
+  // })
   .option('m', {
     alias : 'msg',
     demand: false,
@@ -34,19 +22,20 @@ var argv = require('yargs')
     describe: '提交信息',
     type: 'string'
   })
-  .usage('Usage: push -o [options] -m [options]')
-  .example('push -o daily/0.0.1 -m "update"')
+  .usage('Usage: push -m [options]')
+  .example('push -m "update"')
   .help('h')
   .alias('h', 'help')
   .argv;
 
+var branch = getBranch();
+
+echo('Error: Git push failed', 'test');
 
 if (!which('git')) {
   echo('Sorry, this script requires git');
   exit(1);
 }
-
-// Run external tool synchronously
 if (exec('git add .').code !== 0) {
   echo('Error: Git add failed');
   exit(1);
@@ -55,7 +44,22 @@ if (exec('git commit -am "' + argv.m + '"').code !== 0) {
   echo('Error: Git commit failed');
   exit(1);
 }
-if (exec('git push origin ' + argv.o).code !== 0) {
+if (exec('git push origin ' + branch).code !== 0) {
   echo('Error: Git push failed');
   exit(1);
+}
+
+function getBranch(){
+	var branch = '';
+
+	fs.readFile('.git/HEAD', 'utf8', function (err, data) {
+	    if (err) {
+	        echo('Error: Unable to read .git/HEAD', err);
+	        exit(1);
+	    } else {
+	        branch = data.split(': ').pop().trim().substring(11);
+	    }
+	});
+
+	return branch;
 }
